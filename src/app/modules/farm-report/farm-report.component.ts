598edddaaa4321734c5919da6a1c1637aa4de163
Pabasara Jayakody <hashipaba17@gmail.com>
2021-04-24 12:10:12 +0530

    Farm report updated

--- src/app/modules/farm-report/farm-report.component.ts
+++ src/app/modules/farm-report/farm-report.component.ts
@@ -32,12 +32,12 @@ export class FarmReportComponent implements OnInit {
   generatePDF() {
     var data = document.getElementById('contentToConvert');
     html2canvas(data).then(canvas => {
-      var imgWidth = 208;
+      var imgWidth = 200;
       var imgHeight = canvas.height * imgWidth / canvas.width;
       const contentDataURL = canvas.toDataURL('image/png');
       let pdf = new jsPDF('p', 'mm', 'a4');
-      var position = 0;
-      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+      var position = 6;
+      pdf.addImage(contentDataURL, 'PNG', 6, position, imgWidth, imgHeight);
       pdf.save('newPDF.pdf');
     });
   }
